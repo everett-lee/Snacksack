0112392f01d9dd9a83f5a852d6fad4ba568fea0d
Lee <lee.m.everett@gmail.com>
2022-12-21 14:03:27 +0000

    Remove comments

--- snacksack/src/main/java/com/snacksack/snacksack/SnacksackApplication.java
+++ snacksack/src/main/java/com/snacksack/snacksack/SnacksackApplication.java
@@ -3,9 +3,6 @@ package com.snacksack.snacksack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//curl https://static.wsstack.nn4maws.net/content/v3/menus/137.json  -H "APIKey: YVB5QfeRKUK1+EGvXGjPgQA93reRTUJHsCuQSHR+=="
-// curl https://api.jdwetherspoon.com//api/v2/pubs/137/food  -H "APIKey: YVB5QfeRKUK1+EGvXGjPgQA93reRTUJHsCuQSHR+=="
-
 @SpringBootApplication
 public class SnacksackApplication {
 
